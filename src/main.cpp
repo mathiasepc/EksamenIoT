eeefafa5ff8346e583a44437d54bc82fb6488992
mathias clausen <70236153+mathiasepc@users.noreply.github.com>
2023-09-22 12:23:34 +0200

    Download csv works

--- src/main.cpp
+++ src/main.cpp
@@ -1,93 +1,93 @@
-#include <Arduino.h>
+/*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+*********/
+
+// Libraries for SD card
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+
+//DS18B20 libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
-#include <WiFi.h>
+#include <Arduino.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
-float lastTemperature = -999.0;
+// Libraries to get time from NTP Server
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
 
-// GPIO where the DS18B20 is connected to
-const int oneWireBus = 4;
-
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(oneWireBus);
-
-// Pass our oneWire reference to Dallas Temperature sensor
-DallasTemperature sensors(&oneWire);
-AsyncWebServer server(80);
-AsyncWebSocket ws("/ws");
+// Define deep sleep options
+uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+// Sleep for 10 minutes = 600 seconds
+uint64_t TIME_TO_SLEEP = 600;
 
 // Replace with your network credentials
-const char *ssid = "E308";
-const char *password = "98806829";
+const char* ssid     = "E308";
+const char* password = "98806829";
 
-// Create AsyncWebServer object on port 80
+// Define CS pin for the SD card module
+#define SD_CS 5
 
-String readBME280Temperature()
-{
-  // Read temperature as Celsius (the default)
-  float t = sensors.getTempCByIndex(0);
-  // Convert temperature to Fahrenheit
-  // t = 1.8 * t + 32;
-  if (isnan(t))
-  {
-    Serial.println("Failed to read from BME280 sensor!");
-    return "";
-  }
-  else
-  {
-    Serial.println(t);
-    return String(t);
-  }
-}
+//Prototype
+void writeFile(fs::FS &fs, const char * path, const char * message);
+void getReadings();
+void getTimeStamp();
+void logSDCard();
+void appendFile(fs::FS &fs, const char * path, const char * message);
 
-// Funktion til at håndtere WebSocket-data
-void handleWebSocketData(AsyncWebSocketClient *client, uint8_t *data, size_t len)
-{
-  String command = String((char *)data);
+// Save reading number on RTC memory
+RTC_DATA_ATTR int readingID = 0;
 
-  if (command == "get_temperature")
-  {
-    float temperatureC = sensors.getTempCByIndex(0);
-    String temperatureString = String(temperatureC);
-    client->text(temperatureString);
-  }
-}
+String dataMessage;
 
-void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type,
-             void *arg, uint8_t *data, size_t len)
-{
-  switch (type)
-  {
-  case WS_EVT_CONNECT:
-    Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
-    break;
-  case WS_EVT_DISCONNECT:
-    Serial.printf("WebSocket client #%u disconnected\n", client->id());
-    break;
-  case WS_EVT_DATA:
-    // Kald separat funktion for at håndtere WebSocket-data
-    handleWebSocketData(client, data, len);
-    break;
-  case WS_EVT_PONG:
-  case WS_EVT_ERROR:
-    break;
-  }
-}
+// Data wire is connected to ESP32 GPIO 21
+#define ONE_WIRE_BUS 21
+// Setup a oneWire instance to communicate with a OneWire device
+OneWire oneWire(ONE_WIRE_BUS);
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
 
-void initWebSocket()
-{
-  ws.onEvent(onEvent);
-  server.addHandler(&ws);
-}
+AsyncWebServer server(80);
 
-void setup()
-{
-  // Start the Serial Monitor
+// Temperature Sensor variables
+float temperature;
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+// Variables to save date and time
+String formattedDate;
+String dayStamp;
+String timeStamp;
+
+void setup() {
+  // Start serial communication for debugging purposes
   Serial.begin(115200);
-  // Start the DS18B20 sensor
-  sensors.begin();
+
+  // Connect to Wi-Fi network with SSID and password
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
+
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
 
   // Initialize SPIFFS
   if (!SPIFFS.begin())
@@ -96,49 +96,144 @@ void setup()
     return;
   }
 
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    delay(1000);
-    Serial.println("Connecting to WiFi..");
+  // Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
   }
 
-  // Print ESP32 Local IP Address
-  Serial.println(WiFi.localIP());
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
 
-  initWebSocket();
-
-  // Route for root / web page
+    // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(SPIFFS, "/index.html"); });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send_P(200, "text/plain", readBME280Temperature().c_str()); });
+    { request->send(SPIFFS, "/index.html"); });
 
-  // Start server
+  // Set up the web server
+  server.on("/download_csv", HTTP_GET, [](AsyncWebServerRequest *request){
+    File file = SD.open("/data.txt", FILE_READ);
+    if (file) {
+      request->send(file, "data.csv", "text/csv");
+      file.close();
+    } else {
+      request->send(404, "text/plain", "File not found");
+    }
+  });
+
   server.begin();
+
+  // Enable Timer wake_up
+  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
+
+  // Start the DallasTemperature library
+  sensors.begin(); 
+
+  getReadings();
+  getTimeStamp();
+  logSDCard();
+  
+  // Increment readingID on every new reading
+  readingID++;
+  
+  // Start deep sleep
+  // Serial.println("DONE! Going to sleep now.");
+  // esp_deep_sleep_start(); 
 }
 
-void loop()
-{
-  //Henter temperatur
-  sensors.requestTemperatures();
-  float temperatureC = sensors.getTempCByIndex(0);
+void loop() {
+  // The ESP32 will be in deep sleep
+  // it never reaches the loop()
+}
 
-  // Tjek for temperaturændring
-  if (temperatureC != lastTemperature)
-  {
-    ws.cleanupClients();
+// Function to get temperature
+void getReadings(){
+  sensors.requestTemperatures(); 
+  temperature = sensors.getTempCByIndex(0); // Temperature in Celsius
+  //temperature = sensors.getTempFByIndex(0); // Temperature in Fahrenheit
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+}
 
-    Serial.println(temperatureC);
-    
-    //send til client
-    String temperatureString = String(temperatureC);
-    ws.textAll(temperatureString);
-
-    // Opdater lastTemperature
-    lastTemperature = temperatureC;
+// Function to get date and time from NTPClient
+void getTimeStamp() {
+  while(!timeClient.update()) {
+    timeClient.forceUpdate();
   }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+  formattedDate = timeClient.getFormattedDate();
+  Serial.println(formattedDate);
 
-  delay(1000);
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  dayStamp = formattedDate.substring(0, splitT);
+  Serial.println(dayStamp);
+  // Extract time
+  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  Serial.println(timeStamp);
+}
+
+// Write the sensor readings on the SD card
+void logSDCard() {
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + 
+                String(temperature) + "\r\n";
+  Serial.print("Save data: ");
+  Serial.println(dataMessage);
+  appendFile(SD, "/data.txt", dataMessage.c_str());
+}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
+}
+
+// Append data to the SD card (DON'T MODIFY THIS FUNCTION)
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
 }
