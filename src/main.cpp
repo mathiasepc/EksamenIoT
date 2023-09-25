2e4cc316f5f23a1e70d1859e2bc3550cdb84bad9
mathias clausen <70236153+mathiasepc@users.noreply.github.com>
2023-09-25 10:11:53 +0200

    Fik deepsleep til at virke. Den sleeper efter 5 min

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /*********
   Rui Santos
-  Complete project details at https://randomnerdtutorials.com  
+  Complete project details at https://randomnerdtutorials.com
 *********/
 
 // Libraries for SD card
@@ -8,7 +8,7 @@
 #include "SD.h"
 #include <SPI.h>
 
-//DS18B20 libraries
+// DS18B20 libraries
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <Arduino.h>
@@ -21,23 +21,29 @@
 #include <WiFiUdp.h>
 
 // Define deep sleep options
-uint64_t uS_TO_S_FACTOR = 1000000;  // Conversion factor for micro seconds to seconds
+uint64_t uS_TO_S_FACTOR = 1000000; // Conversion factor for micro seconds to seconds
 // Sleep for 10 minutes = 600 seconds
 uint64_t TIME_TO_SLEEP = 600;
 
 // Replace with your network credentials
-const char* ssid     = "E308";
-const char* password = "98806829";
+const char *ssid = "E308";
+const char *password = "98806829";
+
+
+// Define variables to keep track of time
+unsigned long previousMillis = 0; // Store the previous millis
+const unsigned long interval = 300000; // 5 minutes in milliseconds
+
 
 // Define CS pin for the SD card module
 #define SD_CS 5
 
-//Prototype
-void writeFile(fs::FS &fs, const char * path, const char * message);
+// Prototype
+void writeFile(fs::FS &fs, const char *path, const char *message);
 void getReadings();
 void getTimeStamp();
 void logSDCard();
-void appendFile(fs::FS &fs, const char * path, const char * message);
+void appendFile(fs::FS &fs, const char *path, const char *message);
 
 // Save reading number on RTC memory
 RTC_DATA_ATTR int readingID = 0;
@@ -48,7 +54,7 @@ String dataMessage;
 #define ONE_WIRE_BUS 21
 // Setup a oneWire instance to communicate with a OneWire device
 OneWire oneWire(ONE_WIRE_BUS);
-// Pass our oneWire reference to Dallas Temperature sensor 
+// Pass our oneWire reference to Dallas Temperature sensor
 DallasTemperature sensors(&oneWire);
 
 AsyncWebServer server(80);
@@ -65,7 +71,8 @@ String formattedDate;
 String dayStamp;
 String timeStamp;
 
-void setup() {
+void setup()
+{
   // Start serial communication for debugging purposes
   Serial.begin(115200);
 
@@ -73,7 +80,8 @@ void setup() {
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     Serial.print(".");
   }
@@ -97,51 +105,57 @@ void setup() {
   }
 
   // Initialize SD card
-  SD.begin(SD_CS);  
-  if(!SD.begin(SD_CS)) {
+  SD.begin(SD_CS);
+  if (!SD.begin(SD_CS))
+  {
     Serial.println("Card Mount Failed");
     return;
   }
   uint8_t cardType = SD.cardType();
-  if(cardType == CARD_NONE) {
+  if (cardType == CARD_NONE)
+  {
     Serial.println("No SD card attached");
     return;
   }
   Serial.println("Initializing SD card...");
-  if (!SD.begin(SD_CS)) {
+  if (!SD.begin(SD_CS))
+  {
     Serial.println("ERROR - SD card initialization failed!");
-    return;    // init failed
+    return; // init failed
   }
 
   // If the data.txt file doesn't exist
   // Create a file on the SD card and write the data labels
   File file = SD.open("/data.txt");
-  if(!file) {
+  if (!file)
+  {
     Serial.println("File doens't exist");
     Serial.println("Creating file...");
     writeFile(SD, "/data.txt", "Reading ID, Date, Hour, Temperature \r\n");
   }
-  else {
-    Serial.println("File already exists");  
+  else
+  {
+    Serial.println("File already exists");
   }
   file.close();
 
-    // Route for root / web page
+  // Route for root / web page
   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-    { request->send(SPIFFS, "/index.html"); });
+            { request->send(SPIFFS, "/index.html"); });
 
   // Set up the web server
-  server.on("/download_csv", HTTP_GET, [](AsyncWebServerRequest *request){
+  server.on("/download_csv", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
     File file = SD.open("/data.txt", FILE_READ);
     if (file) {
       request->send(file, "data.csv", "text/csv");
       file.close();
     } else {
       request->send(404, "text/plain", "File not found");
-    }
-  });
+    } });
 
-  server.on("/clear_csv", HTTP_POST, [](AsyncWebServerRequest *request){
+  server.on("/clear_csv", HTTP_POST, [](AsyncWebServerRequest *request)
+            {
     if (SD.remove("/data.txt")) {
       File file = SD.open("/data.txt", FILE_WRITE);
       if (file) {
@@ -152,8 +166,7 @@ void setup() {
       }
     } else {
       request->send(500, "text/plain", "Failed to delete CSV file");
-    }
-  });
+    } });
 
   server.begin();
 
@@ -161,37 +174,47 @@ void setup() {
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
 
   // Start the DallasTemperature library
-  sensors.begin(); 
+  sensors.begin();
 
   getReadings();
   getTimeStamp();
   logSDCard();
-  
+
   // Increment readingID on every new reading
   readingID++;
-  
-  // Start deep sleep
-  // Serial.println("DONE! Going to sleep now.");
-  // esp_deep_sleep_start(); 
 }
 
-void loop() {
-  // The ESP32 will be in deep sleep
-  // it never reaches the loop()
+void loop()
+{
+   // Get the current time
+  unsigned long currentMillis = millis();
+
+  // Check if it's time to run esp_deep_sleep_start()
+  if (currentMillis - previousMillis >= interval) {
+    // Save the current time as the new "previous" time
+    previousMillis = currentMillis;
+
+    // Run your code
+    Serial.println("DONE! Going to sleep now.");
+    esp_deep_sleep_start();
+  }
 }
 
 // Function to get temperature
-void getReadings(){
-  sensors.requestTemperatures(); 
+void getReadings()
+{
+  sensors.requestTemperatures();
   temperature = sensors.getTempCByIndex(0); // Temperature in Celsius
-  //temperature = sensors.getTempFByIndex(0); // Temperature in Fahrenheit
+  // temperature = sensors.getTempFByIndex(0); // Temperature in Fahrenheit
   Serial.print("Temperature: ");
   Serial.println(temperature);
 }
 
 // Function to get date and time from NTPClient
-void getTimeStamp() {
-  while(!timeClient.update()) {
+void getTimeStamp()
+{
+  while (!timeClient.update())
+  {
     timeClient.forceUpdate();
   }
   // The formattedDate comes with the following format:
@@ -205,13 +228,14 @@ void getTimeStamp() {
   dayStamp = formattedDate.substring(0, splitT);
   Serial.println(dayStamp);
   // Extract time
-  timeStamp = formattedDate.substring(splitT+1, formattedDate.length()-1);
+  timeStamp = formattedDate.substring(splitT + 1, formattedDate.length() - 1);
   Serial.println(timeStamp);
 }
 
 // Write the sensor readings on the SD card
-void logSDCard() {
-  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," + 
+void logSDCard()
+{
+  dataMessage = String(readingID) + "," + String(dayStamp) + "," + String(timeStamp) + "," +
                 String(temperature) + "\r\n";
   Serial.print("Save data: ");
   Serial.println(dataMessage);
@@ -219,34 +243,44 @@ void logSDCard() {
 }
 
 // Write to the SD card (DON'T MODIFY THIS FUNCTION)
-void writeFile(fs::FS &fs, const char * path, const char * message) {
+void writeFile(fs::FS &fs, const char *path, const char *message)
+{
   Serial.printf("Writing file: %s\n", path);
 
   File file = fs.open(path, FILE_WRITE);
-  if(!file) {
+  if (!file)
+  {
     Serial.println("Failed to open file for writing");
     return;
   }
-  if(file.print(message)) {
+  if (file.print(message))
+  {
     Serial.println("File written");
-  } else {
+  }
+  else
+  {
     Serial.println("Write failed");
   }
   file.close();
 }
 
 // Append data to the SD card (DON'T MODIFY THIS FUNCTION)
-void appendFile(fs::FS &fs, const char * path, const char * message) {
+void appendFile(fs::FS &fs, const char *path, const char *message)
+{
   Serial.printf("Appending to file: %s\n", path);
 
   File file = fs.open(path, FILE_APPEND);
-  if(!file) {
+  if (!file)
+  {
     Serial.println("Failed to open file for appending");
     return;
   }
-  if(file.print(message)) {
+  if (file.print(message))
+  {
     Serial.println("Message appended");
-  } else {
+  }
+  else
+  {
     Serial.println("Append failed");
   }
   file.close();
